e60902318277d7a5654cc5156d679679b878e658
Junwon Lee <63298243+cpprhtn@users.noreply.github.com>
2024-07-12 10:38:09 +0900

    fix: UnicodeDecodeError

--- main.py
+++ main.py
@@ -20,16 +20,18 @@ st.image(image)
 col1, col2 = st.columns([1, 1])
 with col1:
     read_csv_header = 0 if st.checkbox("csv header") else None
+    large_cav = st.checkbox("Read Large Scale CSV")
 with col2:
+    read_csv_index = 0 if st.checkbox("csv index") else None
     transpose_data = st.checkbox("transpose?")
 
 
-if st.checkbox("Read Large Scale CSV"):
+if large_cav:
     st.session_state["LCSV"] = True
     uploaded_file = st.text_input("Write file path", "test.csv")
     if uploaded_file is not None:
         try:
-            df = pd.read_csv(uploaded_file, header=read_csv_header)
+            df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index)
             st.write(df.head())
             save_df(df, "df", st.session_state["LCSV"])
         except FileNotFoundError:
@@ -39,13 +41,20 @@ if st.checkbox("Read Large Scale CSV"):
                      
                      ArrowTypeError: ("Expected bytes, got a 'float' object", 'Conversion failed for column 2 with type object')
                      ''')
+        except UnicodeDecodeError:
+            df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index, encoding="CP949")
+            st.write(df.head())
+            save_df(df, "df", st.session_state["LCSV"])
         
 else:
     st.session_state["LCSV"] = False
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, header=read_csv_header, low_memory=False)
+        try:
+            df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index, low_memory=False)
+        except UnicodeDecodeError:
+            df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index, low_memory=False, encoding="CP949")
         if transpose_data:
             df = df.transpose()
         st.subheader('DataFrame Header')
