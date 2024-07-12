3fcf72caff35640e049a7c572afd27caa48c49bb
Junwon Lee <63298243+cpprhtn@users.noreply.github.com>
2024-07-12 14:29:43 +0900

    feat: support read parquet in main

--- main.py
+++ main.py
@@ -20,32 +20,45 @@ st.image(image)
 col1, col2 = st.columns([1, 1])
 with col1:
     read_csv_header = 0 if st.checkbox("csv header") else None
-    large_cav = st.checkbox("Read Large Scale CSV")
+    large_csv = st.checkbox("Read Large Scale CSV")
 with col2:
     read_csv_index = 0 if st.checkbox("csv index") else None
     transpose_data = st.checkbox("transpose?")
 
 
-if large_cav:
+if large_csv:
     st.session_state["LCSV"] = True
-    uploaded_file = st.text_input("Write file path", "test.csv")
-    if uploaded_file is not None:
-        try:
-            df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index)
-            st.write(df.head())
-            save_df(df, "df", st.session_state["LCSV"])
-        except FileNotFoundError:
-            st.error('FileNotFoundError: Specified file not found') 
-        except pyarrow.lib.ArrowTypeError:
-            st.error('''Try to check "csv header"
-                     
-                     ArrowTypeError: ("Expected bytes, got a 'float' object", 'Conversion failed for column 2 with type object')
-                     ''')
-        except UnicodeDecodeError:
-            df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index, encoding="CP949")
-            st.write(df.head())
-            save_df(df, "df", st.session_state["LCSV"])
-        
+    uploaded_file = st.text_input("Write file path", "")
+    if st.button("load latest csv"):
+        df = pd.read_parquet("./cache/df.parquet")
+        if read_csv_header == 0:
+            df.columns = df.iloc[0]
+            df.drop([0], inplace=True)
+        if read_csv_index == 0:
+            df.index = df.iloc[:, 0]
+            df.drop([df.columns[0]], axis=1, inplace=True)
+        st.write(df.head())
+    
+   
+    
+    else:
+        if uploaded_file is not None:
+            try:
+                df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index, low_memory=False)
+                st.write(df.head())
+                save_df(df, "df", st.session_state["LCSV"])
+            except FileNotFoundError:
+                st.error('FileNotFoundError: Specified file not found') 
+            except pyarrow.lib.ArrowTypeError:
+                st.error('''Try to check "csv header"
+                        
+                        ArrowTypeError: ("Expected bytes, got a 'float' object", 'Conversion failed for column 2 with type object')
+                        ''')
+            except UnicodeDecodeError:
+                df = pd.read_csv(uploaded_file, header=read_csv_header, index_col=read_csv_index, encoding="CP949", low_memory=False)
+                st.write(df.head())
+                save_df(df, "df", st.session_state["LCSV"])
+            
 else:
     st.session_state["LCSV"] = False
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
@@ -61,4 +74,3 @@ else:
         st.dataframe(df.head())
 
         save_df(df, "df", st.session_state["LCSV"])
-        
